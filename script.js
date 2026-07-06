cd670ffb8d3dda308dabef474235482082d7f530
Greg Franz <168317593+BuildAnythingWithCode@users.noreply.github.com>
2026-07-07 01:54:46 +0800

    get it mostly working

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const button = {
   closeModalLazy: document.querySelector('#close-modal-lazy'),
   closeModalX: document.querySelector('#close-modal-x'),
   addToDo: document.querySelector('#add-to-do'),
+  defaultProjectSidebar: document.querySelector('#default-project-sidebar'),
 };
 
 // Elements
@@ -18,6 +19,7 @@ const element = {
   defaultProject: document.querySelector('#default-project'),
   customProject: document.querySelector('#custom-project'),
   choiceOfProject: document.querySelectorAll("input[name='project']"),
+  sidebar: document.querySelector('#sidebar'),
 };
 
 element.choiceOfProject.forEach((radio) => {
@@ -36,23 +38,41 @@ element.choiceOfProject.forEach((radio) => {
 const listOfShitToDo = [];
 
 // Constructor Function
-function ThingToDo(title, description, dueDate, priority) {
+function ThingToDo(title, description, dueDate, priority, project) {
   this.title = title;
   this.description = description;
   this.dueDate = dueDate;
   this.priority = priority;
+  this.project = project;
 }
 
 // Regular Functions
-function addThingToDo(title, description, dueDate, priority) {
+function addThingToDo(title, description, dueDate, priority, project) {
   title = document.querySelector('#the-shit').value;
   description = document.querySelector('#the-details-of-the-shit').value;
   dueDate = document.querySelector('#due-date').value;
   priority = document.querySelector('input[name="importance"]:checked').value;
-  const newThingToDo = new ThingToDo(title, description, dueDate, priority);
+  const selectedProject = document.querySelector(
+    'input[name="project"]:checked',
+  ).value;
+  if (selectedProject === 'other') {
+    project = element.customProject.value;
+    const newProject = document.createElement('button');
+    newProject.textContent = element.customProject.value;
+    button.defaultProjectSidebar.after(newProject);
+  } else {
+    project = 'Default Project';
+  }
+  const newThingToDo = new ThingToDo(
+    title,
+    description,
+    dueDate,
+    priority,
+    project,
+  );
   listOfShitToDo.push(newThingToDo);
   const newEntry = document.createElement('p');
-  newEntry.textContent = `Title: ${title}, Description: ${description}, Due Date: ${dueDate}, Priority: ${priority}`;
+  newEntry.textContent = `Title: ${title}, Description: ${description}, Due Date: ${dueDate}, Priority: ${priority}, Project: ${project}`;
   element.shitToDoSection.appendChild(newEntry);
 }
 
